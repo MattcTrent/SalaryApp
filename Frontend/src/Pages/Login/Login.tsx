da35b54dbd563a8b5ac15c014f753b808509fa67
Matt Trent <matt_trent@hotmail.com>
2023-08-07 10:50:14 +0100

    login updated to check for unauthorized not bad request

--- Frontend/src/Pages/Login/Login.tsx
+++ Frontend/src/Pages/Login/Login.tsx
@@ -84,7 +84,7 @@ export const action: ActionFunction = async ({ request, params }) => {
       return null;
     }
 
-    if (error.response.status === 400) {
+    if (error.response.status === 401) {
       if (error.response.data.message) {
         toast.error(error.response.data.message);
       } else if (error.message) {
