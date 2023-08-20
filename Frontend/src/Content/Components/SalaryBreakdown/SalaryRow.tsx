9649dc4a61d005d74151c50d3c9fef57359edc91
Matt Trent <matt_trent@hotmail.com>
2023-08-20 10:44:31 +0100

    Changed Salary Show to only show the selected column

--- Frontend/src/Content/Components/SalaryBreakdown/SalaryRow.tsx
+++ Frontend/src/Content/Components/SalaryBreakdown/SalaryRow.tsx
@@ -1,11 +1,14 @@
 import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell";
 import NumberHelper from "../../Utils/NumberHelpers";
+import { Showcolumn } from "../../Enums/ShowColum";
+
 import styles from "./SalaryRow.module.css";
 
 interface DataRowProps {
   rowName: string;
   rowValue: number;
+  showColumn: string;
 }
 
 export const SalaryRow = (props: DataRowProps) => {
@@ -21,24 +24,22 @@ export const SalaryRow = (props: DataRowProps) => {
 
   return (
     <TableRow className={styles.row}>
-      <TableCell className={styles.titleCell} align="left">
-        {props.rowName}
-      </TableCell>
-      <TableCell align="left">
+      <TableCell align="left" className={styles.titleCell}>{props.rowName}</TableCell>
+      {props.showColumn === Showcolumn.YEARLY ? <TableCell align="left">
         {NumberHelper.ToCurrencyString(yearlyValue)}
-      </TableCell>
-      <TableCell align="left">
+      </TableCell> : null}
+      {props.showColumn === Showcolumn.MONTHLY ? <TableCell align="left">
         {NumberHelper.ToCurrencyString(props.rowValue)}
-      </TableCell>
-      <TableCell align="left">
+      </TableCell> : null}
+      {props.showColumn === Showcolumn.WEEKLY ? <TableCell align="left">
         {NumberHelper.ToCurrencyString(weeklyValue)}
-      </TableCell>
-      <TableCell align="left">
+      </TableCell> : null}
+      {props.showColumn === Showcolumn.DAILY ? <TableCell align="left">
         {NumberHelper.ToCurrencyString(dailyValue)}
-      </TableCell>
-      <TableCell align="left">
+      </TableCell> : null}
+      {props.showColumn === Showcolumn.Hourly ? <TableCell align="left">
         {NumberHelper.ToCurrencyString(hourlyValue)}
-      </TableCell>
+      </TableCell> : null}
     </TableRow>
   );
 };
