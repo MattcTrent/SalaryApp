3ca06a95ff256df3e4e927116dfccdf4fc09416d
Matt Trent <matt_trent@hotmail.com>
2023-08-20 10:45:34 +0100

    changed salary breakdown to allow properyly setting what column to display

--- Frontend/src/Content/Components/SalaryBreakdown/SalaryBreakdown.tsx
+++ Frontend/src/Content/Components/SalaryBreakdown/SalaryBreakdown.tsx
@@ -8,12 +8,23 @@ import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell";
 import TableBody from "@mui/material/TableBody";
 import Paper from "@mui/material/Paper";
+import { useState } from "react";
+import SelectInput from "../UI/Input/SelectInput/SelectInput";
+import { ShowColumns } from "../../Enums/ShowColum";
 
 interface SalaryTableProps {
   salaryBreakdown: ISalaryBreakdown | null;
 }
 
 export const SalaryBreakdown = (props: SalaryTableProps) => {
+  const [showColumn, setShowColumn] = useState<string>("Monthly");
+
+  function showColumnChangeHandler(
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) {
+    setShowColumn(event.target.value);
+  }
+
   return (
     <TableContainer className={styles.container} component={Paper}>
       {props.salaryBreakdown ? (
@@ -31,17 +42,22 @@ export const SalaryBreakdown = (props: SalaryTableProps) => {
             <TableHead>
               <TableRow className={styles.headerRow}>
                 <TableCell align="center"></TableCell>
-                <TableCell align="left">Yearly</TableCell>
-                <TableCell align="left">Monthly</TableCell>
-                <TableCell align="left">Weekly</TableCell>
-                <TableCell align="left">Daily</TableCell>
-                <TableCell align="left">Hourly</TableCell>
+                <TableCell align="left">
+                  <SelectInput
+                    id="inpShowColumn"
+                    name="showColumn"
+                    onChange={showColumnChangeHandler}
+                    value={showColumn}
+                    values={ShowColumns}
+                  ></SelectInput>
+                </TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
               <SalaryRow
                 rowName="Salary"
                 rowValue={props.salaryBreakdown.monthlySalary}
+                showColumn={showColumn}
               />
               <SalaryRow
                 rowName="Salary Post Salary Sacrifice"
@@ -49,36 +65,46 @@ export const SalaryBreakdown = (props: SalaryTableProps) => {
                   props.salaryBreakdown.monthlySalary -
                   props.salaryBreakdown.salarySacrifice
                 }
+                showColumn={showColumn}
               />
               <SalaryRow
                 rowName="Salary Sacrifice Scheme"
                 rowValue={props.salaryBreakdown.salarySacrifice}
+                showColumn={showColumn}
               />
               <SalaryRow
                 rowName="Pension"
                 rowValue={props.salaryBreakdown.pension}
+                showColumn={showColumn}
               />
               <SalaryRow
                 rowName="Tax Free"
                 rowValue={props.salaryBreakdown.taxFree}
+                showColumn={showColumn}
               />
-              <SalaryRow rowName="Tax" rowValue={props.salaryBreakdown.tax} />
-              <SalaryRow rowName="NI" rowValue={props.salaryBreakdown.nI} />
+              <SalaryRow rowName="Tax" rowValue={props.salaryBreakdown.tax}
+                showColumn={showColumn} />
+              <SalaryRow rowName="NI" rowValue={props.salaryBreakdown.nI}
+                showColumn={showColumn} />
               <SalaryRow
                 rowName="Student Loan"
                 rowValue={props.salaryBreakdown.studentFinance}
+                showColumn={showColumn}
               />
               <SalaryRow
                 rowName="Take Home"
                 rowValue={props.salaryBreakdown.takehome}
+                showColumn={showColumn}
               />
               <SalaryRow
                 rowName="Bills"
                 rowValue={props.salaryBreakdown.bills}
+                showColumn={showColumn}
               />
               <SalaryRow
                 rowName="After Bills"
                 rowValue={props.salaryBreakdown.takehomeAfterBills}
+                showColumn={showColumn}
               />
             </TableBody>
           </Table>
