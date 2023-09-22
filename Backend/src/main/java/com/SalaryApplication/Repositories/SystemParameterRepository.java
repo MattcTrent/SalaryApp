f165057e078b42f6b5c8c046c168dbdb1272ebe6
Matt Trent <matt_trent@hotmail.com>
2023-09-22 14:42:28 +0100

    added getby to repo

--- Backend/src/main/java/com/SalaryApplication/Repositories/SystemParameterRepository.java
+++ Backend/src/main/java/com/SalaryApplication/Repositories/SystemParameterRepository.java
@@ -1,6 +1,7 @@
 package com.SalaryApplication.Repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,7 +10,10 @@ import com.SalaryApplication.Models.SystemParameter;
 import com.SalaryApplication.Models.Enums.SystemParameterGroupEnum;
 
 @Repository
-public interface SystemParameterRepository extends JpaRepository<SystemParameter, Integer>{
-    
+public interface SystemParameterRepository extends JpaRepository<SystemParameter, Integer> {
+
     List<SystemParameter> findByParameterGroup(SystemParameterGroupEnum parameterGroup);
+
+    Optional<SystemParameter> findByParameterGroupAndName(SystemParameterGroupEnum parameterGroup,
+            String parameterName);
 }
