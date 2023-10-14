6f4977a9ce70b0d92db8bbdaaf6b1c8599495e01
Matt Trent <matt_trent@hotmail.com>
2023-10-14 15:18:04 +0100

    fixed axio config

--- frontend/src/api/AxiosConfig.tsx
+++ frontend/src/api/AxiosConfig.tsx
@@ -4,13 +4,13 @@ export function getConfiguredAxios() {
   const token: string | null = null;
   const axiosClient = axios.create({
     baseURL: import.meta.env.VITE_REST_API_URL,
+    withCredentials: true,
     headers: {
       "Content-Type": "application/json",
       Accept: "application/json",
-      Authorization: token,
+      Authorization: token ? `Bearer ${token}` : "",
     },
     timeout: 30000,
-    withCredentials: false,
   });
   return axiosClient;
 }
